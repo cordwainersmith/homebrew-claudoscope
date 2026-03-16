d2a074704998e000b5718ae29aa45030f8d23b58
liranb@jfrog.com <liranb@jfrog.com>
2026-03-16 14:33:17 +0200

    Update claudoscope to v0.4.0

--- Casks/claudoscope.rb
+++ Casks/claudoscope.rb
@@ -1,8 +1,8 @@
 cask "claudoscope" do
-  version "0.3.9"
-  sha256 "124afea2de5be2b3412a969add0272d3fba1eaa94eda278d29b8e0bbe2bdc784"
+  version "0.4.0"
+  sha256 "ed72440a774d244ea5b458f872810b3bb97dc9953903d1ba3c456d314b35fa10"
 
-  url "https://dl.claudoscope.com/v0.3.9/Claudoscope.dmg",
+  url "https://dl.claudoscope.com/v0.4.0/Claudoscope.dmg",
       verified: "dl.claudoscope.com/"
   name "Claudoscope"
   desc "macOS menu bar companion for Claude Code sessions"
